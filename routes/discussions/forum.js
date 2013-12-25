8ec97e170b5c8b690e6440ad25701e803962033f
Maria <themarianne@gmail.com>
2013-12-25 15:08:25 +0200

    pagination started

--- routes/discussions/forum.js
+++ routes/discussions/forum.js
@@ -6,7 +6,7 @@ var models = require('../../models'),
 module.exports = function(req,res) {
 //    var resource = new PostForumResource();
     var subject_id = req.params[0];
-    var page = req.query.page || 0,
+    var page = req.query.page || 1,
         limit = 10,
         offset = (page - 1) * limit;
     async.waterfall([
@@ -63,31 +63,11 @@ module.exports = function(req,res) {
             url:req.url,
             posts: page_posts || [],
             post_groups: post_groups,
-            count: main_posts.length
+            count: main_posts.length,
+            page: page,
+            next: Number(page + 1),
+            prev: Number(page - 1)
         });
     });
 
 };
-//    models.Subject.findById(subject_id,function(err,subject) {
-//        if(err)
-//            res.render('500.ejs',{error:err});
-//        else {
-//            if(!subject)
-//                res.redirect('/discussions');
-//            else
-//                res.render('discussion_list_by_subject.ejs', {
-//                    layout: false,
-//                    tag_name:req.query.tag_name,
-//                    subject:subject,
-//                    title:"דיונים",
-//                    logged: req.isAuthenticated(),
-//                    big_impressive_title: "",
-//                    user: req.user,
-//                    avatar:req.session.avatar_url,
-//                    user_logged: req.isAuthenticated(),
-//                    url:req.url,
-//                    tab:'discussions'
-//                });
-//        }
-//    });
-//};
