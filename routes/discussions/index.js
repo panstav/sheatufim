3f049be613023a4d2a4c6e8068d5d3f3151c2812
saar stauber <saarsta@gmail.com>
2014-01-08 19:46:45 +0200

    start redesign discussion page

--- routes/discussions/index.js
+++ routes/discussions/index.js
@@ -7,6 +7,8 @@ module.exports = function(router)
 
     router.all(/\/subject\/([0-9a-f]+)\/?/,require('./subject'));
 
+    router.all(/\/test\/([0-9a-f]+)\/?/,require('./test'));
+
     router.all(/\/([0-9a-f]+)\/?$/,require('./main'));
 
     router.all(/\/([0-9a-f]+)\/posts\/([0-9a-f]+)\/?$/,require('./post'));
---
+++ routes/discussions/test.js
@@ -0,0 +1,71 @@
+var models = require('../../models')
+    , DiscussionResource = require('../../api/discussions/DiscussionResource.js')
+    , async = require('async')
+    , notifications = require('../../api/notifications.js');
+
+module.exports = function (req, res) {
+    var resource = new DiscussionResource();
+    var user = req.user;
+
+    async.parallel([
+        // get the discussion object
+        function (cbk) {
+            models.Discussion.findById(req.params[0])
+                .populate('creator_id', {
+                    '_id': 1,
+                    'first_name': 1,
+                    'last_name': 1})
+                .exec(cbk);
+        },
+
+        function (cbk) {
+            models.Discussion.update({ _id: req.params[0]}, {$inc: {view_counter: 1}}, cbk);
+        }
+    ],
+        function (err, results) {
+            if (err)
+                return res.handle500(err);
+
+            var discussion = results[0];
+            if (!discussion)
+                return res.redirect('/discussions');
+
+            if (!discussion.isUserAllowed(req.user))
+                return res.redirect('/discussions');
+            // populate 'is follower' , 'grade object' ...
+            resource.get_discussion(discussion, user, function (err, discussion) {
+
+                if (err)
+                    return res.render('500.ejs', {error: err});
+                res.setHeader("Expires", "0");
+
+                var ejs = 'discussion_new.ejs';
+
+                res.render(ejs, {
+                    title: "דיון",
+                    discussion_id: req.params[0],
+                    subject_id: req.query.subject_id,
+                    tab: 'discussions',
+                    discussion: discussion,
+                    fb_description: discussion.text_field_preview,
+                    fb_title: discussion.title,
+                    fb_image: discussion.image_field && discussion.image_field.url,
+                    user: user,
+                    meta: {
+                        type: req.app.get('facebook_app_name') + ':discussion',
+                        id: discussion.id,
+                        image: ((discussion.image_field_preview && discussion.image_field_preview.url) ||
+                            (discussion.image_field && discussion.image_field.url)),
+                        title: discussion && discussion.title,
+                        description: discussion.text_field_preview || discussion.text_field,
+                        link: discussion && ('/discussions/' + discussion.id)
+                    }
+                });
+
+                //update all notifications of user that connected to this object
+                if (user)
+                    notifications.updateVisited(user, req.path);
+            });
+        });
+
+};
