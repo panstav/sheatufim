fd8c5defc64c1d891b1786fd8ff80db0d13439d6
saar stauber <saarsta@gmail.com>
2014-01-13 14:00:07 +0200

    change text

--- routes/navigation/home.js
+++ routes/navigation/home.js
@@ -4,7 +4,7 @@ module.exports = function(req, res){
 
     res.render('index_new.ejs', {
          welcome_pre_title:doc.welcome_pre_title || "ברוכים הבאים",
-         welcome_title: doc.welcome_title || "לזירות השיח",
+         welcome_title: doc.welcome_title || "למעגלי השיח",
          text: doc.text || "נולום ארווס סאפיאן - פוסיליס קוויס, אקווזמן קוואזי במר מודוף. אודיפו בלאסטיק מונופץ קליר, בנפת נפקט למסון וענוף להאמית קרהשק סכעיט דז מא, מנכם למטכין נשואי מנורך גולר מונפרר סוברט לורם שבצק יהול."
      });
 }
