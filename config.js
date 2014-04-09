64dee223b8e8b0027ce4651e80c27f9481ea421a
saarsta <saarsta@gmail.com>
2014-04-09 20:44:55 +0300

    fix config bug

--- config.js
+++ config.js
@@ -1,44 +1,30 @@
 var config = {};
 
-// facebook app params
-config.fb_auth_params = {
-    appId : process.env['FACEBOOK_APPID'] || '175023072601087',
-    appSecret: process.env['FACEBOOK_SECRET'] || '5ef7a37e8a09eca5ee54f6ae56aa003f',
-    appName: process.env['FACEBOOK_APPNAME'] || 'uru_dev',
-    callback: config.ROOT_PATH + '/account/facebooklogin',
-    scope: 'email,publish_actions',
-    failedUri: '/noauth'
-};
-
-config.fb_general_params = {
-    fb_title: 'עורו',
-    fb_description:  "עורו היא תנועה חברתית לייצוג הרוב בישראל. אנו מאמינים שבעידן שבו אנו חיים, כולנו מסוגלים וזכאים להשתתף בקבלת ההחלטות. לכן, עורו מנהלת פלטפורמה לדיון ציבורי, יסודי ואפקטיבי שיוביל שינוי בסדר היום. אצלנו, האג'נדה מוכתבת מלמטה.",
-    fb_image: 'http://site.e-dologic.co.il/philip_morris/Xls_script/uru_mailing/logo.jpg'
-}
-
 config.upload_dir = require('path').join(__dirname,'..','public','cdn');
-
-// amazon s3 credentials
-//config.s3_creds = {
-//    key: 'AKIAJM4EPWE637IGDTQA',
-//    secret: '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',
-//    bucket: 'uru'
-//};
 
 config.sendgrid_user = 'app2952775@heroku.com';
 config.sendgrid_key = 'a0oui08x';
 config.systemEmail = 'maagal@sheatufim.org.il';
 config.systemEmailName = 'SHEATUFIM';
 
-config.SMTPUsername = process.env.SMTP_USERNAME || 'saar@uru.org.il';
-config.SMTPPassword = process.env.SMTP_PASSWORD || 'uruuru11';
-
-
-
 config.DB_URL = process.env['MONGOLAB_URI'] || 'mongodb://localhost/idemos';
 config.ROOT_PATH = process.env.ROOT_PATH || 'http://dev.empeeric.com';
 
+// facebook app params
+config.fb_auth_params = {
+    appId : process.env['FACEBOOK_APPID'] || '',
+    appSecret: process.env['FACEBOOK_SECRET'] || '',
+    appName: process.env['FACEBOOK_APPNAME'] || '',
+    callback: config.ROOT_PATH + '/account/facebooklogin',
+    scope: 'email,publish_actions',
+    failedUri: '/noauth'
+};
 
+config.fb_general_params = {
+    fb_title: '',
+    fb_description:  '',
+    fb_image: ''
+}
 
 /**
  * Static fils configuration
