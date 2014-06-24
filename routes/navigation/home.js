b8b64e1b76e0b833c1fa49c661792e066857f4e2
MariaTolstov <themarianne@gmail.com>
2014-06-24 12:27:09 +0300

    fake + fixes

--- routes/navigation/home.js
+++ routes/navigation/home.js
@@ -2,7 +2,7 @@ module.exports = function(req, res){
     var models = require('../../models');
     var doc = models.General.getGeneral();
 
-    res.render('index_fake.ejs', {
+    res.render('index_new.ejs', {
          welcome_pre_title:doc.welcome_pre_title || "ברוכים הבאים",
          welcome_title: doc.welcome_title || "למעגלי השיח",
          text: doc.text || "נולום ארווס סאפיאן - פוסיליס קוויס, אקווזמן קוואזי במר מודוף. אודיפו בלאסטיק מונופץ קליר, בנפת נפקט למסון וענוף להאמית קרהשק סכעיט דז מא, מנכם למטכין נשואי מנורך גולר מונפרר סוברט לורם שבצק יהול."
